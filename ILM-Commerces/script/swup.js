c6306524b4049181af93e38b346dc229bd72fe99
Nyssa0 <hassinaayachi@hotmail.com>
2024-02-18 00:56:53 +0100

    :fire: remove swup & files

--- ILM-Commerces/script/swup.js
+++
@@ -1,7 +0,0 @@
-import Swup from 'https://unpkg.com/swup@4?module';
-import SwupParallelPlugin from 'https://unpkg.com/@swup/parallel-plugin@0?module';
-
-const swup = new Swup({
-    containers: ["#swup"],
-    plugins: [new SwupParallelPlugin()]
-});
