eb2bf61795ee753cb5d91a4c690c1e3890d9b21f
Nyssa0 <hassinaayachi@hotmail.com>
2024-02-18 01:02:03 +0100

    :sparkles: add icons on the map and close info window

--- ILM-Commerces/script/map.js
+++ ILM-Commerces/script/map.js
@@ -1,43 +1,72 @@
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 48.82242275511905, lng: 2.263443834627964},
-        zoom: 14,
+        zoom: 15,
         mapId: '72acca18b847dc01',
         mapTypeControl: false
     });
 
+    let openInfoWindow;
+
     const iconBase =
-        "./assets/";
+        "./assets/mapIcons/";
     const icons = {
-        parking: {
-            icon: iconBase + "parking_lot_maps.png",
-        },
-        library: {
-            icon: iconBase + "library_maps.png",
-        },
-        Alimentation: {
+        alimentation: {
             icon: iconBase + "alimentation.png",
-            scaledSize: new google.maps.Size(50, 50) // Taille personnalisée pour l'alimentation
         },
-        Equipement_de_la_maison: {
-            icon: "Equipement_de_la_maison.png",
+        equipement_de_la_maison: {
+            icon: iconBase + "equipement.png",
         },
-        Beauté_et_le_bien_être: {
-            icon: "Beauté_et_le_bien_être.png",
+        beauté_et_le_bien_être: {
+            icon: iconBase + "bienetre.png",
+        },
+        restauration___bars___tabacs: {
+            icon: iconBase + "restaurants.png",
+        },
+        cordonniers__serruriers__laveries_et_pressings: {
+            icon: iconBase + "cordonniers.png",
+        },
+        service_aux_particuliers: {
+            icon: iconBase + "particuliers.png",
+        },
+        loisirs_et_le_tourisme: {
+            icon: iconBase + "loisirs.png",
+        },
+        equipement_de_la_personne: {
+            icon: iconBase + "alimentation.png",
+        },
+        automobile_et_deux_roues: {
+            icon: iconBase + "voiture.png",
+        },
+        hôtels: {
+            icon: iconBase + "hotel.png",
+        },
+        entretien__rénovation_et_équipement: {
+            icon: iconBase + "entretien.png",
+        },
+        assurances_et_banques: {
+            icon: iconBase + "banque.png",
+        },
+        non_renseignée: {
+            icon: iconBase + "alimentation.png",
+        },
+        santé: {
+            icon: iconBase + "sante.png",
+
         }
+
 
     };
 
     async function displayCommerces() {
-        const response = await fetch("https://data.issy.com/api/explore/v2.1/catalog/datasets/touslescommercesdissy-les-moulineaux-feuille1/records");
+        const response = await fetch("https://data.issy.com/api/explore/v2.1/catalog/datasets/touslescommercesdissy-les-moulineaux-feuille1/records?limit=100");
         const commerces = await response.json();
-        console.log(commerces.results);
 
         commerces.results.forEach(commerce => {
-            console.log(commerce.categorie)
+            const commerce_category = commerce.categorie.toLowerCase().replaceAll(" ", "_").replaceAll("-", "_").replaceAll(",", "_");
             const marker = new google.maps.Marker({
                 position: new google.maps.LatLng(commerce.geo_point_2d.lat, commerce.geo_point_2d.lon),
-                icon: icons[commerce.categorie] ? icons[commerce.categorie].icon : '',
+                icon: icons[commerce_category] ? icons[commerce_category].icon : '',
                 map: map,
                 animation: google.maps.Animation.DROP
             });
@@ -46,9 +75,9 @@ function initMap() {
                 `<div id="content">
                     <div id="siteNotice">
                     </div>
-                    <h1 id="firstHeading" class="firstHeading">${commerce.nom_commerce}</h1>
+                    <h4 id="firstHeading" class="firstHeading">${commerce.nom_commerce}</h4>
                     <div id="bodyContent">
-                    <p><b>${commerce.nom_commerce}</b>, ${commerce.localisation}.</p>
+                    <p>${commerce.localisation}.</p>
                     </div>
                 </div>`;
 
@@ -58,11 +87,15 @@ function initMap() {
             });
 
             marker.addListener("click", () => {
+                if (openInfoWindow) {
+                    openInfoWindow.close();
+                }
+
                 infowindow.open({
                     anchor: marker,
                     map,
                 });
-                // toggleBounce(marker);
+                openInfoWindow = infowindow;
             });
 
         });
